51ddcd129c2759b00b7d9826204c85464dea027e
BartoszCzernal <bartekczernal@gmail.com>
2020-08-05 11:48:15 +0200

    Modify model Lesson and create serializers, views and urls for that model

--- tutoring/lessons/models.py
+++ tutoring/lessons/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Lesson(models.Model):
     student = models.CharField(max_length=50)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     online = models.BooleanField()
-    additional_info = models.CharField(max_length=300)
+    additional_info = models.CharField(max_length=300, null=True, blank=True)
 
 
 
--- tutoring/lessons/views.py
+++ tutoring/lessons/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Lesson
+from .serializers import LessonSerializer
 
-# Create your views here.
+
+class LessonList(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all().order_by('start_date')
+    serializer_class = LessonSerializer
+
+
+class LessonDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all().order_by('start_date')
+    serializer_class = LessonSerializer
