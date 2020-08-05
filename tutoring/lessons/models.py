7793fd414f1c29eee67d3b854d678a4f7d322de1
BartoszCzernal <bartekczernal@gmail.com>
2020-08-05 11:01:03 +0200

    Create model Lesson

--- tutoring/lessons/models.py
+++ tutoring/lessons/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Lesson(models.Model):
-    student = models.CharField(max_length=30)
+    student = models.CharField(max_length=50)
     start_date = models.DateField()
     end_date = models.DateField()
+    online = models.BooleanField()
     additional_info = models.CharField(max_length=300)
 
-    class Meta:
-        abstract = True
+
+
+
 
